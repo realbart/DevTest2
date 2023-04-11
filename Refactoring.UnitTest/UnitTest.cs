631f4f525fe88681a0f0532817da6fce91718d5b
Bart Kemps ☺ <b.kemps@rdc.nl>
2023-04-11 17:28:14 +0200

    Add Rhombus

--- Refactoring.UnitTest/UnitTest.cs
+++ Refactoring.UnitTest/UnitTest.cs
@@ -19,6 +19,9 @@
         private const double RectangleHeight = 23d;
         private const double RectangleWidth = 67d;
         private const double RectangleSurfaceArea = 1541d;
+        private const double RhombusHeight = 13d;
+        private const double RhombusWidth = 34d;
+        private const double RhombusSurfaceArea = 442d;
 
         private class ConsoleMock : IConsole
         {
@@ -46,6 +49,7 @@
                 $"create circle {CircleRadius}",
                 $"create square {SquareSide}",
                 $"create rectangle {RectangleHeight} {RectangleWidth}",
+                $"create rhombus {RhombusHeight} {RhombusWidth}",
                 "calculate",
                 "print",
                 "reset",
@@ -63,6 +67,7 @@
             Assert.IsTrue(output.Contains("- create circle {double} (create a new circle)"));
             Assert.IsTrue(output.Contains("- create rectangle {height} {width} (create a new rectangle)"));
             Assert.IsTrue(output.Contains("- create triangle {height} {width} (create a new triangle)"));
+            Assert.IsTrue(output.Contains("- create rhombus {height} {base} (create a new rhombus)"));
             Assert.IsTrue(output.Contains("- print (print the calculated surface areas)"));
             Assert.IsTrue(output.Contains("- calculate (calulate the surface areas of the created shapes)"));
             Assert.IsTrue(output.Contains("- reset (reset)"));
@@ -71,10 +76,12 @@
             Assert.IsTrue(output.Contains("Circle created!"));
             Assert.IsTrue(output.Contains("Square created!"));
             Assert.IsTrue(output.Contains("Rectangle created!"));
+            Assert.IsTrue(output.Contains("Rhombus created!"));
             Assert.IsTrue(output.Contains($"triangle surface area is {TriangleSurfaceArea}"));
             Assert.IsTrue(output.Contains($"circle surface area is {CircleSurfaceArea}"));
             Assert.IsTrue(output.Contains($"square surface area is {SquareSurfaceArea}"));
             Assert.IsTrue(output.Contains($"rectangle surface area is {RectangleSurfaceArea}"));
+            Assert.IsTrue(output.Contains($"rhombus surface area is {RhombusSurfaceArea}"));
             Assert.IsTrue(output.Contains("Reset state!!"));
             Assert.IsTrue(output.Contains("There are no surface areas to print"));
         }
